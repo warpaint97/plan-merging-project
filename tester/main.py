bcfbe93c80bf8796ccad734a4e43de07a8a16f18
warpaint97 <warpaint97@users.noreply.github.com>
2021-12-14 04:24:22 +0100

    added validity checker

--- tester/main.py
+++ tester/main.py
@@ -3,7 +3,7 @@ import funcs
 
 # ENTER PATHS (with cwd ./plan-merging-project)
 #################################################################################
-merger = "encodings/merger_waiting_choice_rules_improved2.lp"
+merger = "encodings/merger_plan_switching_greedy3.lp"
 benchmark_id = 14
 #################################################################################
 
@@ -18,10 +18,14 @@ if __name__ == "__main__":
     #################################################################################
     # run clingo
     m, s, t = clg.solve(bm_program, merger)
-    clg.load_viz(m)
+
+    m = m.replace("occurs","occurs_")
+    m, s, t = clg.solve(m, "encodings/merger_waiting_choice_rules_improved3.lp")
 
     m = m.replace("occurs","occurs_")
     m, s, t = clg.solve(m, "encodings/merger_plan_switching+wait.lp")
+
+    m, s, t = clg.solve(m, "encodings/validity_checker.lp")
 
     # run clingo incrementally
     #m, s, t = clg.isolve(bm_program, merger, "max_waits", lambda x: 10*(x+1), 10)
