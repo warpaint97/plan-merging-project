b41f90c82457a2afddb242a84643254586ac0b83
warpaint97 <bishop.aaron@yahoo.com>
2022-02-10 02:00:00 +0100

    merger class improved

--- tester/final.py
+++ tester/final.py
@@ -9,11 +9,18 @@ if __name__ == "__main__":
     # initialize merger
     merger = Merger("encodings/sequential", "benchmark_data")
 
-    for i in range(1,len(benchmarks)):
-        benchmark_id = i-1
-        benchmark = benchmarks[benchmark_id]
-        if i not in [22,23,25]:
-            merger.merge(benchmark, vizualize=True, save_data=True)
+    def merge(benchmark_id):
+        benchmark = benchmarks[benchmark_id-1]
+        merger.merge(benchmark, vizualize=True, save_data=True)
+
+    def mergeAll():
+        for i in range(1,len(benchmarks)):
+            benchmark_id = i-1
+            benchmark = benchmarks[benchmark_id]
+            if i not in [22,23,25]:
+                merger.merge(benchmark, vizualize=True, save_data=True)
+
+    merge(14)
 
     #m, _ = merger.switchPlans(merger.getBenchmarkModel(benchmark))
     #m, _ = merger.wait(m)
